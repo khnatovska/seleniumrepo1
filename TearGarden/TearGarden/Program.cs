9d51e5d3c7c5807dac0379416935eb650bca04e5
kgnatovska <k.hnatovska@gmail.com>
2016-11-15 22:56:35 +0200

    does not work still

---
+++ TearGarden/TearGarden/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace TearGarden
+{
+    class Program
+    {
+        static IWebDriver driver = new ChromeDriver();
+        static WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        public string CoreIP = "10.35.178.196";
+        static string CoreUsername = "administrator";
+        static string CorePassword = "123asdQ";
+
+        static void Main(string[] args)
+        {
+            //OpenBrowser(CoreIP, CoreUsername, CorePassword);
+            
+        }
+
+        public static void OpenBrowser(string coreIP, string coreUsername, string corePassword)
+        {
+            string CoreURL = String.Format("https://{0}:{1}@{2}:8006/apprecovery/admin/", coreUsername, corePassword, coreIP);
+            driver.Navigate().GoToUrl(CoreURL);
+            driver.Manage().Window.Maximize();
+        }
+
+        public static void OpenBrowser(IWebDriver webdriver, string coreIP, string coreUsername, string corePassword)
+        {
+            string CoreURL = String.Format("https://{0}:{1}@{2}:8006/apprecovery/admin/", coreUsername, corePassword, coreIP);
+            webdriver.Navigate().GoToUrl(CoreURL);
+            webdriver.Manage().Window.Maximize();
+        }
+
+        public static void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ TearGarden/TearGarden/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace TearGarden
+{
+    class Repository
+    {
+        private IWebDriver Driver;
+        private WebDriverWait Wait;
+        private string CoreIP = "10.35.178.196";
+        private string CoreUsername = "administrator";
+        private string CorePassword = "123asdQ";
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            Program.OpenBrowser(Driver, CoreIP, CoreUsername, CorePassword);
+        }
+
+        [Test]
+        public void OpenRepositoriesPage()
+        {
+            //start: any page. find Repositories in More Navigation menu, open Repositories page, verify URL
+            IWebElement more = Driver.FindElement(By.CssSelector("#coreNavigationMore span.ui-navigation-core-menu-icon"));
+            more.Click();
+            IWebElement repoInMore = Driver.FindElement(By.CssSelector("#coreNavigationMoreMenu > li:nth-child(5) > a"));
+            repoInMore.Click();
+            Wait.Until(ExpectedConditions.UrlContains("Repository"));
+            Assert.IsTrue(Driver.Url.Contains("Repository"));
+        }
+    }
+}
